13a61ffbc6824b49adc600d509de26f4e8525a06
Beret <c445352742@126.com>
2020-02-06 00:49:26 +0800

    optimize code

--- words/main.js
+++ words/main.js
@@ -6,11 +6,32 @@ $(function () {
     left: 37,
     right: 39,
   }
-  var index = [];
-  var time = 500;
+  var index = -1;
+  var time = 300;
   var flag = 'up';
   var enable = true;
+  var lib = JSON.parse(JSON.stringify(top100));
+  var libname = 'top100'
+  r();
   down('init');
+
+  $('.btn').click(function () {
+    if (libname == 'top100') {
+      lib = JSON.parse(JSON.stringify(all))
+      libname = 'all'
+      index = -1
+      $('.lib').html('all')
+      r();
+      down();
+    } else {
+      lib = JSON.parse(JSON.stringify(top100))
+      index = -1
+      libname = 'top100'
+      $('.lib').html('top100[' + lib.length + ']')
+      r();
+      down();
+    }
+  })
 
   $(document).keydown(function (e) {
     if (!enable) { return; }
@@ -28,7 +49,9 @@ $(function () {
       case key.right:
         left();
         break;
-      default: return;
+      default:
+        enable = true
+        return;
     }
   })
   function down(p) {
@@ -41,9 +64,12 @@ $(function () {
       enable = true;
     }, time)
     setTimeout(function () {
-      r();
-      $('.line1').html(top100[index[0]].word)
+      index++;
+      index = index % lib.length
+      $('.page').html(index + 1)
+      $('.line1').html(lib[index].word)
     }, time / 2)
+
     flag = 'up'
   }
   function up() {
@@ -54,8 +80,10 @@ $(function () {
       enable = true;
     }, time)
     setTimeout(function () {
-      index.shift()
-      $('.line1').html(top100[index[0]].word)
+      index--;
+      $('.page').html(index + 1)
+      index = (index+lib.length) % lib.length
+      $('.line1').html(lib[index].word)
     }, time / 2)
     flag = 'up'
   }
@@ -67,8 +95,8 @@ $(function () {
       enable = true;
     }, time)
     setTimeout(function () {
-      flag == 'up' ? $('.line1').html(top100[index[0]].word) : ($('.line1').html(top100[index[0]].phonetic),
-        $('.line2').html(top100[index[0]].meaning))
+      flag == 'up' ? $('.line1').html(lib[index].word) : ($('.line1').html(lib[index].phonetic.trim()),
+        $('.line2').html(lib[index].meaning))
     }, time / 2)
     turnOver();
   }
@@ -76,12 +104,21 @@ $(function () {
     $('.line1').html('')
     $('.line2').html('')
   }
-  function r() {
-    var num = Math.floor(Math.random() * 3);
-    index.unshift(num);
-    index.length = 10;
-  }
   function turnOver() {
     flag = flag == 'up' ? 'down' : 'up'
+  }
+  function r() {
+    var i = 0;
+    var num = 0;
+    var arr = [];
+    var length = lib.length
+
+    for (i = 0; i < length; i++) {
+      num = Math.floor(Math.random() * lib.length)
+      arr.push(lib[num])
+      lib.splice(num, 1)
+    }
+    lib = arr;
+    console.log(lib)
   }
 })
--- words/words.js
+++ words/words.js
@@ -2,6 +2,17 @@ var top100 = []
 top100 = a();
 var c = 3
 function a() {
+  // return[ {
+  //   word: '0',
+  //   phonetic: '/fəˈnetɪk/',
+  //   meaning: '语音（学）的',
+  //   flag: 'forgotten'
+  // },  {
+  //   word: '1',
+  //   phonetic: '/fəˈnetɪk/',
+  //   meaning: '语音（学）的',
+  //   flag: 'forgotten'
+  // }]
   return [{
     word: 'fanatic',
     phonetic: '/fəˈnætɪk/',
@@ -9,26 +20,261 @@ function a() {
     flag: 'forgotten'
   }, {
     word: 'phonetic',
-    phonetic: ' /fəˈnetɪk/',
+    phonetic: '/fəˈnetɪk/',
     meaning: '语音（学）的',
     flag: 'forgotten'
   }, {
-    word: 'xcvvd',
-    phonetic: '【1】',
-    meaning: '含义',
+    word: 'censure',
+    phonetic: '/ˈsenʃər/ ',
+    meaning: 'verb 严厉斥责，正式谴责 noun 斥责，谴责',
+    flag: 'forgotten'
+  }, {
+    word: 'shanks',
+    phonetic: '/ˈsenʃər/ ',
+    meaning: 'n.小腿；长腿的人（shank的复数）    v.从梗上枯萎脱落；用球杆击（shank的第三人称单数）    n. (Shanks)人名；',
+    flag: 'forgotten'
+  }, {
+    word: 'burrowed',
+    phonetic: '/ˈbɜːroʊ/',
+    meaning: 'v. 挖地洞，挖通道；依偎  n. （动物的，尤指兔子挖的）洞穴，地洞',
+    flag: 'forgotten'
+  }, {
+    word: 'era',
+    phonetic: '/ˈɪrə,ˈerə/ ',
+    meaning: 'n. 纪元，时代',
+    flag: 'forgotten'
+  }, {
+    word: 'swing',
+    phonetic: '/swɪŋ/',
+    meaning: 'v. 摇摆；悬挂；<br/>（在轴上）转动；移转（船、飞机）至各方位点；（突然）转向；绞死；行进；甩；改变（情绪或意见）；获得；对……有决定性影响；'+
+      '成功地完成；节奏感强'+ 'n. 摇摆；明显改变；秋千；挥杆动作；摇摆乐；音律；轻快的行动；（板球）侧飞球；（政客）多处逗留的旅行；击打；涨落 adj. 强节奏爵士音乐的；决定性的',
+    flag: 'forgotten'
+  }, {
+    word: 'exile',
+    phonetic: '/ˈeksaɪl,ˈeɡzaɪl/ ',
+    meaning: 'n.vt. 放逐，流放',
+    flag: 'forgotten'
+  }, {
+    word: 'dread',
+    phonetic: '/ˈeksaɪl,ˈeɡzaɪl/ ',
+    meaning: 'v.n.adj.（adjective） 惧怕，担心；敬畏 ',
+    flag: 'forgotten'
+  }, {
+    word: 'drone',
+    phonetic: '/droʊn/',
+    meaning: 'n. 雄蜂；嗡嗡的声音；懒惰者    n. 无人机（非正式）',
+    flag: 'forgotten'
+  }, {
+    word: 'covet',
+    phonetic: '/ˈkʌvət/ ',
+    meaning: 'vt. 垂涎；觊觎 vi. 垂涎；觊觎',
+    flag: 'forgotten'
+  }, {
+    word: 'shebang',
+    phonetic: '/ʃɪˈbæŋ/ ',
+    meaning: 'n. 工作；事情；住所',
+    flag: 'forgotten'
+  }, {
+    word: 'reassuring',
+    phonetic: '/ˌriːəˈʃʊrɪŋ/',
+    meaning: 'adj. 安心的；可靠的；鼓气的    v. 使放心',
+    flag: 'forgotten'
+  }, {
+    word: 'farce',
+    phonetic: '/fɑːrs/',
+    meaning: 'n. 闹剧；胡闹；笑剧',
+    flag: 'forgotten'
+  }, {
+    word: 'intruder',
+    phonetic: '/ɪnˈtruːdə/',
+    meaning: 'n. 侵入者；干扰者',
+    flag: 'forgotten'
+  }, {
+    word: 'quilt',
+    phonetic: '/kwɪlt/',
+    meaning: 'n. 被子；棉被 vt. 东拼西凑地编；加软衬料后缝制',
+    flag: 'forgotten'
+  }, {
+    word: 'marshes',
+    phonetic: '//',
+    meaning: 'n. [地理] 沼泽（marsh的复数）',
+    flag: 'forgotten'
+  }, {
+    word: 'stake',
+    phonetic: '/steɪk/',
+    meaning: 'n. 桩，棍子；奖金 vt. 资助',
+    flag: 'forgotten'
+  }, {
+    word: 'seep',
+    phonetic: '/siːp/',
+    meaning: 'v. （液体）渗漏，渗透',
+    flag: 'forgotten'
+  }, {
+    word: 'vast',
+    phonetic: '/væst/',
+    meaning: 'adj. 广阔的；巨大的 n. 浩瀚；',
+    flag: 'forgotten'
+  }, {
+    word: 'stud',
+    phonetic: '/stʌd/',
+    meaning: 'n. 种马；大头钉；饰纽；',
+    flag: 'forgotten'
+  }, {
+    word: 'studded',
+    phonetic: '/ˈstʌdɪd/',
+    meaning: 'v. 镶嵌,打上装饰钉',
+    flag: 'forgotten'
+  }, {
+    word: 'coombe',
+    phonetic: '/ku:m/',
+    meaning: 'n. 峡谷 coomb',
+    flag: 'forgotten'
+  }, {
+    word: 'brook',
+    phonetic: '/brʊk/',
+    meaning: 'n. 小溪 v. 容忍',
+    flag: 'forgotten'
+  }, {
+    word: 'offspring',
+    phonetic: '/ˈɔːfsprɪŋ/',
+    meaning: 'n. 后代；产物',
+    flag: 'forgotten'
+  }, {
+    word: 'meteorological',
+    phonetic: '/ˌmiːtiərəˈlɑːdʒɪkl/',
+    meaning: 'adj. 气象的；气象学的',
+    flag: 'forgotten'
+  }, {
+    word: 'amateur',
+    phonetic: '/ˈæmətər/',
+    meaning: 'n. 业余爱好者；生手，外行 adj. 业余爱好的；',
+    flag: 'forgotten'
+  }, {
+    word: 'demonstrate',
+    phonetic: '/ˈdemənstreɪt/',
+    meaning: 'vt. 证明；展示；论证  vi. 示威',
+    flag: 'forgotten'
+  }, {
+    word: 'initiate',
+    phonetic: '/ɪˈnɪʃieɪt/',
+    meaning: 'vt. 开始;发起；使初步了解 n. 开始；新加入者，接受初步知识者',
+    flag: 'forgotten'
+  }, {
+    word: 'balm',
+    phonetic: '/bɑːm/',
+    meaning: 'n. 香油；镇痛软膏；',
+    flag: 'forgotten'
+  }, {
+    word: 'residue',
+    phonetic: '/ˈrezɪduː/',
+    meaning: 'n. 残渣；剩余',
+    flag: 'forgotten'
+  }, {
+    word: 'thermal',
+    phonetic: '/ˈθɜːrml/',
+    meaning: 'adj. 热的；保热的 n. 上升的热气流',
+    flag: 'forgotten'
+  }, {
+    word: 'take time',
+    phonetic: '/ˈθɜːrml/',
+    meaning: '从容的',
+    flag: 'forgotten'
+  }, {
+    word: 'wretch',
+    phonetic: '/retʃ/',
+    meaning: 'n. 可怜的人；卑鄙的人',
+    flag: 'forgotten'
+  }, {
+    word: 'friction',
+    phonetic: '/ˈfrɪkʃn/',
+    meaning: 'n. 摩擦(力)',
+    flag: 'forgotten'
+  }, {
+    word: 'reanimate',
+    phonetic: '/riːˈænɪmeɪt/',
+    meaning: 'vt. 鼓舞；使复活；使恢复生气',
+    flag: 'forgotten'
+  }, {
+    word: 'glow',
+    phonetic: '/ɡloʊ/',
+    meaning: 'v. 发出微弱而稳定的光；脸发红；容光焕发；喜形于色',
+    flag: 'forgotten'
+  }, {
+    word: 'anticipate',
+    phonetic: '/ænˈtɪsɪpeɪt/',
+    meaning: 'v. 预料，预期；预见，预计（并做准备）',
+    flag: 'forgotten'
+  }, {
+    word: 'sheer',
+    phonetic: '/ʃɪr/',
+    meaning: 'adj. 绝对的；透明的；峻峭的；纯粹的  '+
+    ' adv. 完全；陡峭地 vi. 偏航  vt. 使偏航',
+    flag: 'forgotten'
+  }, {
+    word: 'vape',
+    phonetic: '/veɪp/',
+    meaning: 'v. 吸电子烟',
+    flag: 'forgotten'
+  }, {
+    word: 'high time',
+    phonetic: '//',
+    meaning: '应该立刻去做某事',
+    flag: 'forgotten'
+  }, {
+    word: 'humid',
+    phonetic: '/ˈhjuːmɪd/',
+    meaning: 'adj. 潮湿的；湿润的；多湿气的',
+    flag: 'forgotten'
+  }, {
+    word: 'oracle',
+    phonetic: '/ˈɒrəkl/',
+    meaning: 'n. 神谕；预言；圣人',
+    flag: 'forgotten'
+  }, {
+    word: 'linger',
+    phonetic: '/ˈlɪŋɡər/',
+    meaning: 'vi. 苟延残喘；磨蹭; 徘徊；  vt. 消磨；缓慢度过',
+    flag: 'forgotten'
+  }, {
+    word: 'indolent',
+    phonetic: '/ˈɪndələnt/',
+    meaning: 'adj. 懒惰的；无痛的',
+    flag: 'forgotten'
+  }, {
+    word: 'archipelago',
+    phonetic: '/ˌɑːkɪˈpeləɡəʊ/',
+    meaning: 'n. 群岛，列岛；多岛的海区',
+    flag: 'forgotten'
+  }, {
+    word: 'rehabilitation',
+    phonetic: '/ˌriːəˌbɪlɪˈteɪʃn/',
+    meaning: 'n. 复原',
+    flag: 'forgotten'
+  }, {
+    word: 'hue',
+    phonetic: '/hjuː/',
+    meaning: 'n. 色彩；色度；色调；叫声',
+    flag: 'forgotten'
+  }, {
+    word: 'scowl',
+    phonetic: '/skaʊl/',
+    meaning: 'v.怒视（某人或某物）  n.怒容',
+    flag: 'forgotten'
+  }, {
+    word: 'spectral',
+    phonetic: '/ˈspektrəl/',
+    meaning: '[光] 光谱的；幽灵的',
     flag: 'forgotten'
   }]
 }
+
+
 var all = b();
 function b() {
   return [{
-    word: '风暴大剑',
-    phonetic: '',
-    meaning: 1
-  }, {
-    word: '风暴大剑',
-    phonetic: '',
-    meaning: 1
-  }]
+    word: 'spectral',
+    phonetic: '/ˈspektrəl/',
+    meaning: '[光] 光谱的；幽灵的',
+    flag: 'forgotten'
+  }];
 }
-console.log('words')
